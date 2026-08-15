a629f9552f379edba23d7df8b8904941989a2691
Noel Sebulele <noelsebulele@gmail.com>
2026-08-15 23:35:38 +0200

    Add customer management applications source files

---
+++ Form1.Designer.vb
@@ -0,0 +1,329 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCustomerManagementSystem
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCustomerID = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lstReceipt = New System.Windows.Forms.ListBox()
+        Me.btnLoadData = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 67)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Customer ID:"
+        '
+        'txtCustomerID
+        '
+        Me.txtCustomerID.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.txtCustomerID.Location = New System.Drawing.Point(162, 56)
+        Me.txtCustomerID.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtCustomerID.Multiline = True
+        Me.txtCustomerID.Name = "txtCustomerID"
+        Me.txtCustomerID.Size = New System.Drawing.Size(143, 38)
+        Me.txtCustomerID.TabIndex = 1
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.txtName.Location = New System.Drawing.Point(469, 56)
+        Me.txtName.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtName.Multiline = True
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(212, 38)
+        Me.txtName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(389, 67)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 21)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Name:"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.txtEmail.Location = New System.Drawing.Point(884, 56)
+        Me.txtEmail.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtEmail.Multiline = True
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(355, 38)
+        Me.txtEmail.TabIndex = 5
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmail.Location = New System.Drawing.Point(788, 67)
+        Me.lblEmail.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(56, 21)
+        Me.lblEmail.TabIndex = 4
+        Me.lblEmail.Text = "Email:"
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.txtAddress.Location = New System.Drawing.Point(884, 139)
+        Me.txtAddress.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtAddress.Multiline = True
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(355, 38)
+        Me.txtAddress.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(788, 150)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 21)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Address:"
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.txtPhone.Location = New System.Drawing.Point(469, 139)
+        Me.txtPhone.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtPhone.Multiline = True
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(212, 38)
+        Me.txtPhone.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(389, 150)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(59, 21)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Phone:"
+        '
+        'Label3
+        '
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label3.Location = New System.Drawing.Point(13, 207)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(1226, 466)
+        Me.Label3.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(22, 224)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(139, 21)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Customer Records"
+        '
+        'lstReceipt
+        '
+        Me.lstReceipt.FormattingEnabled = True
+        Me.lstReceipt.ItemHeight = 21
+        Me.lstReceipt.Location = New System.Drawing.Point(27, 255)
+        Me.lstReceipt.Name = "lstReceipt"
+        Me.lstReceipt.Size = New System.Drawing.Size(1180, 382)
+        Me.lstReceipt.TabIndex = 12
+        '
+        'btnLoadData
+        '
+        Me.btnLoadData.BackColor = System.Drawing.Color.Yellow
+        Me.btnLoadData.Location = New System.Drawing.Point(91, 676)
+        Me.btnLoadData.Name = "btnLoadData"
+        Me.btnLoadData.Size = New System.Drawing.Size(141, 38)
+        Me.btnLoadData.TabIndex = 13
+        Me.btnLoadData.Text = "Load Data"
+        Me.btnLoadData.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Yellow
+        Me.btnExit.Location = New System.Drawing.Point(1049, 675)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(115, 38)
+        Me.btnExit.TabIndex = 14
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.Yellow
+        Me.btnSave.Location = New System.Drawing.Point(891, 675)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(115, 38)
+        Me.btnSave.TabIndex = 15
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'btnSearch
+        '
+        Me.btnSearch.BackColor = System.Drawing.Color.Yellow
+        Me.btnSearch.Location = New System.Drawing.Point(555, 676)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(137, 38)
+        Me.btnSearch.TabIndex = 16
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = False
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.BackColor = System.Drawing.Color.Yellow
+        Me.btnUpdate.Location = New System.Drawing.Point(413, 675)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(115, 38)
+        Me.btnUpdate.TabIndex = 17
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = False
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.BackColor = System.Drawing.Color.Yellow
+        Me.btnBrowse.Location = New System.Drawing.Point(268, 676)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(115, 38)
+        Me.btnBrowse.TabIndex = 18
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = False
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.txtSearch.Location = New System.Drawing.Point(162, 139)
+        Me.txtSearch.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtSearch.Multiline = True
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(152, 38)
+        Me.txtSearch.TabIndex = 20
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(22, 150)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(61, 21)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "Search:"
+        '
+        'btnSort
+        '
+        Me.btnSort.BackColor = System.Drawing.Color.Yellow
+        Me.btnSort.Location = New System.Drawing.Point(713, 675)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(137, 38)
+        Me.btnSort.TabIndex = 21
+        Me.btnSort.Text = "Sort"
+        Me.btnSort.UseVisualStyleBackColor = False
+        '
+        'frmCustomerManagementSystem
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1252, 763)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLoadData)
+        Me.Controls.Add(Me.lstReceipt)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtCustomerID)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmCustomerManagementSystem"
+        Me.Text = "Trendwear Clothing"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCustomerID As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lstReceipt As ListBox
+    Friend WithEvents btnLoadData As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btnSort As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,219 @@
+﻿Imports System.IO
+Public Class frmCustomerManagementSystem
+
+    'Structure to hold one customer record
+    Structure Customer
+        Dim CustomerID As Integer
+        Dim Name As String
+        Dim Email As String
+        Dim Phone As String
+        Dim Address As String
+    End Structure
+
+    'Array to store up to 15 customers (IDs 0-14)
+    Dim Customers(14) As Customer
+
+    'Counter to track how many records are loaded
+    Dim RecordCount As Integer = 0
+
+    Private Sub btnLoadData_Click(sender As Object, e As EventArgs) Handles btnLoadData.Click
+        Try
+            'Open the text file
+            Dim sr As New StreamReader("customers.txt")
+            Dim line As String
+            RecordCount = 0
+
+            'Read each line until the end of the file
+            Do While Not sr.EndOfStream
+                line = sr.ReadLine()
+                Dim fields() As String = line.Split(","c)
+
+                'Store each field into the customers array
+                Customers(RecordCount).CustomerID = CInt(fields(0))
+                Customers(RecordCount).Name = fields(1)
+                Customers(RecordCount).Email = fields(2)
+                Customers(RecordCount).Phone = fields(3)
+                Customers(RecordCount).Address = fields(4)
+
+                RecordCount += 1
+            Loop
+
+            sr.Close()
+            'Confirmation Message
+            MessageBox.Show("Data has been succesfully read and ready to display.")
+
+        Catch ex As Exception
+            MessageBox.Show("Error loading file: " & ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        'Clear the listbox first
+        lstReceipt.Items.Clear()
+
+        'Loop through all loaded records
+        For i As Integer = 0 To RecordCount - 1
+            'Show only Customer info in the listbox
+            lstReceipt.Items.Add($"{Customers(i).CustomerID} - {Customers(i).Name} - {Customers(i).Email} - {Customers(i).Phone} - {Customers(i).Address}")
+        Next
+    End Sub
+
+    Private Sub lstReciept_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstReceipt.SelectedIndexChanged
+
+        If lstReceipt.SelectedIndex >= 0 Then
+            'Get the text of the selected item
+
+            Dim selectedText As String = lstReceipt.SelectedItem.ToString()
+            Dim parts() As String = selectedText.Split("-"c)
+            Dim selectedID As Integer = CInt(parts(0).Trim())
+
+
+            'Find the matching record by ID
+            For i As Integer = 0 To RecordCount - 1
+                If Customers(i).CustomerID = selectedID Then
+                    txtCustomerID.Text = Customers(i).CustomerID.ToString()
+                    txtName.Text = Customers(i).Name
+                    txtEmail.Text = Customers(i).Email
+                    txtPhone.Text = Customers(i).Phone
+                    txtAddress.Text = Customers(i).Address
+                    Exit For
+                End If
+            Next
+
+        End If
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Try
+            'Check for empty fields
+            If txtName.Text.Trim() = "" Or txtEmail.Text.Trim() = "" Then
+                MessageBox.Show("Name and Email cannot be empty")
+                Exit Sub
+            End If
+
+            'Use of validation functions
+            If Not IsValidEmail(txtEmail.Text) Then
+                MessageBox.Show("Enter a valid email with @ and .")
+                Exit Sub
+            End If
+
+            If Not IsValidID(txtCustomerID.Text) Then
+                MessageBox.Show("Customer ID must be a positive number")
+                Exit Sub
+            End If
+
+            'Get the Id from the textbox
+            Dim idToUpdate As Integer = CInt(txtCustomerID.Text)
+
+            'Search for the matching record in the array
+            For i As Integer = 0 To RecordCount - 1
+                If Customers(i).CustomerID = idToUpdate Then
+                    'Update the records with new valuesform the textboxes
+                    Customers(i).Name = txtName.Text
+                    Customers(i).Email = txtEmail.Text
+                    Customers(i).Phone = txtPhone.Text
+                    Customers(i).Address = txtAddress.Text
+
+                    'Confirmation message
+                    MessageBox.Show("Records updated succesfully.")
+
+                    'Refresh the listbox so the updated name shows
+                    btnBrowse.PerformClick()
+                    Exit Sub
+                End If
+            Next
+
+            'if no matchoing ID was found
+            MessageBox.Show("Customer ID not found.")
+        Catch ex As Exception
+            MessageBox.Show("Error updating record: " & ex.Message)
+        End Try
+
+        'User-defined validation functions
+
+
+    End Sub
+
+    'User-defined Boolean Function to validate email format
+    Function IsValidEmail(Email As String) As Boolean
+        'Simple check - must have @ and .
+        If Email.Contains("@") And Email.Contains(".") Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Function IsValidID(idText As String) As Boolean
+        Dim id As Integer
+        Return Integer.TryParse(idText, id) AndAlso id > 0
+    End Function
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Try
+            'Open the file for writing (overwriting exixsting content)
+            Dim sw As New StreamWriter("customers.txt")
+
+            'Write each record back to the file
+            For i As Integer = 0 To RecordCount - 1
+                sw.WriteLine($"{Customers(i).CustomerID}, {Customers(i).Name}, {Customers(i).Email}, {Customers(i).Phone}, {Customers(i).Address}")
+            Next
+
+            sw.Close()
+
+            'Confirmation message
+            MessageBox.Show("Data has been saved succesfully and is ready for next use.")
+        Catch ex As Exception
+
+            MessageBox.Show("Error saving file: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim searchTerm As String = txtSearch.Text.Trim().ToLower()
+        lstReceipt.Items.Clear()
+
+        For i As Integer = 0 To RecordCount - 1
+            If Customers(i).Name.ToLower().Contains(searchTerm) Then
+                lstReceipt.Items.Add($"{Customers(i).CustomerID}, {Customers(i).Name}, {Customers(i).Email}, {Customers(i).Phone}, {Customers(i).Address}")
+            End If
+        Next
+
+        If lstReceipt.Items.Count = 0 Then
+            MessageBox.Show("No matching records found.")
+        End If
+    End Sub
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        'Simple bubble sort by Name
+
+        For i As Integer = 0 To RecordCount - 2
+            For j As Integer = i + 1 To RecordCount - 1
+                If String.Compare(Customers(i).Name, Customers(j).Name) > 0 Then
+                    'Swap Records
+
+                    Dim temp As Customer = Customers(i)
+                    Customers(i) = Customers(j)
+                    Customers(j) = temp
+                End If
+            Next
+        Next
+
+        'Refresh the listbox
+        btnBrowse.PerformClick()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Ask user for confirmation
+
+        Dim response As DialogResult
+        response = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        'if they click yes, close the application
+        If response = DialogResult.Yes Then
+            Application.Exit()
+        End If
+
+    End Sub
+End Class
